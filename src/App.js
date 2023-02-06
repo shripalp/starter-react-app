201b3b590221fe154af7265da06b5fb64e088500
shripal <shripalp@gmail.com>
2023-02-06 16:55:56 -0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Info from './Info'
 
 function App() {
   return (
     <div className="App">
       <h1>My name is shripal</h1>
+      <Info />
     </div>
   );
 }
---
+++ src/Info.js
@@ -0,0 +1,10 @@
+function Info() {
+    return (
+        <>
+        <h2>hello</h2>
+        </>
+    )
+
+}
+
+export default Info
